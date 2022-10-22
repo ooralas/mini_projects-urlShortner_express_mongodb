b063939f07f604934cc8a04515c6311610d8f15c
Salar <ooralas@outlook.com>
2022-10-23 00:49:49 +0200

    added dotenv lib & update package.json

--- config/db.js
+++ config/db.js
@@ -1,17 +1,18 @@
+require('dotenv').config()
 const mongoose = require('mongoose')
 const db = process.env.MONGODB_URI
 
-const connectDB = async() => {
-    try {
-        await mongoose.connect(db, {
-            useNewUrlParser: true
-        })
-        console.log("MongoDB is connected");
-    } catch (err) {
-        console.error(err.message);
-        //exit with Failure
-        process.exit(1)
-    }
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+    })
+    console.log('MongoDB is connected')
+  } catch (err) {
+    console.error(err.message)
+    //exit with Failure
+    process.exit(1)
+  }
 }
 
 module.exports = connectDB
